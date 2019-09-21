e5296d7bb21a15e6735d1780a78310450e488910
psz <hwgzpsz@126.com>
2019-09-21 17:34:28 +0800

    spConstant change

--- lib/constant/sp_constants.dart
+++ lib/constant/sp_constants.dart
@@ -1,6 +1,7 @@
 class SpConstants {
-  static const String USER_INFO = "userInfo";
-  static const String USER_ID = "userId";
-  static const String TOKEN = "token";
-  static const String ACCESS_TIME = "accessTime";
+  static const String SP_PREFIX = "Liar_";
+  static const String USER_INFO = SP_PREFIX + "userInfo";
+  static const String USER_ID = SP_PREFIX + "userId";
+  static const String TOKEN = SP_PREFIX + "token";
+  static const String ACCESS_TIME = SP_PREFIX + "accessTime";
 }
